1382a8f092e29407b02fa3b4d858203a605cf19a
Pavel Korolev <dev@borodust.org>
2018-04-18 22:52:07 +0300

    Use simple-vector as atomic-counter for ccl

--- atomic.lisp
+++ atomic.lisp
@@ -11,10 +11,20 @@ should be an accessor form for a struct slot holding an integer."
   #-(or lispworks ccl sbcl) (error "ATOMIC-DECF unimplemented"))
 
 
+#-ccl
 (defstruct (atomic-counter
-             (:constructor %make-atomic-counter (value))
-             #+ccl (:type vector))
+             (:constructor %make-atomic-counter (value)))
   (value 0 :type (unsigned-byte 64)))
+
+
+#+ccl
+(progn
+  (deftype atomic-counter () '(simple-array t 1))
+  (defun* (%make-atomic-counter -> atomic-counter) ((value (unsigned-byte 64)))
+    (make-array 1 :element-type t :initial-element value))
+
+  (defmacro atomic-counter-value (counter)
+    `(svref ,counter 0)))
 
 
 (defun* (make-atomic-counter -> atomic-counter)
@@ -22,6 +32,6 @@ should be an accessor form for a struct slot holding an integer."
   (%make-atomic-counter value))
 
 
-(defun* (decrement-counter -> (unsigned-byte 64)) (counter)
+(defun* (decrement-counter -> (unsigned-byte 64)) ((counter atomic-counter))
   "Returns previous value"
   (%atomic-decf (atomic-counter-value counter)))
