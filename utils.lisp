16593ece6e94030337b6d18131486d7ec11ba738
Pavel Korolev <dev@borodust.org>
2018-07-28 12:18:38 +0300

    Don't apply optimization until :flow-optimized feature is present

--- utils.lisp
+++ utils.lisp
@@ -1,5 +1,8 @@
 (cl:in-package :cl-flow)
 
 
+#+flow-optimized
 (alexandria:define-constant +optimize-form+ '(optimize (speed 3) (safety 0) (debug 0))
   :test #'equal)
+#-flow-optimized
+(alexandria:define-constant +optimize-form+ '(optimize) :test #'equal)
